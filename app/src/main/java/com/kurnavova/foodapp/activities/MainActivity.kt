8f5887eadb810f298d87317b0c5a7b8c8fef05b4
Simona Kurňavová <kurnavova.simona@gmail.com>
2021-01-17 23:33:49 +0100

    Search bar added

--- app/src/main/java/com/kurnavova/foodapp/activities/MainActivity.kt
+++ app/src/main/java/com/kurnavova/foodapp/activities/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.kurnavova.foodapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.navigation.NavigationView
 import com.kurnavova.foodapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.menu_search).actionView as SearchView
+        searchView.apply {
+            // Assumes current activity is the searchable activity
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            setIconifiedByDefault(false) // Do not iconify the widget; expand it by default
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d("SEARCH", "Search query: $query")
+                text.text = query
+                return false
+            }
+            override fun onQueryTextChange(query: String?): Boolean {
+                Log.d("SEARCH", "Search query changed: $query")
+                return true
+            }
+        })
+        return true
     }
 }
